376b28f62ab717ffb3cc55bfa252d70e0fe9312a
Dae-sang Cho <cdsleaf@naver.com>
2017-04-01 20:56:35 +0900

    add event detail modal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { ModalController, Platform, NavController, NavParams, ViewController  } from 'ionic-angular';
 
-import { EventProgram } from '../../app/eventProgram/eventProgram';
 import { EventService } from '../../app/eventProgram/eventProgram.service';
 
-import { AngularFire, FirebaseListObservable} from 'angularfire2';
+import { FirebaseListObservable} from 'angularfire2';
 
 /*
   Generated class for the Home page.
@@ -22,7 +21,11 @@ export class HomePage implements OnInit{
   programList: FirebaseListObservable<any>;
   selectTab: string = "onGoing";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private eventService: EventService) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private eventService: EventService,
+    public modalCtrl: ModalController) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
@@ -36,4 +39,54 @@ export class HomePage implements OnInit{
     this.getEvents(this.selectTab);
   }
 
+  openModal(characterNum) {
+
+    let modal = this.modalCtrl.create(ModalContentPage, characterNum);
+    modal.present();
+  }
+
+}
+
+@Component({
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          Description
+        </ion-title>
+        <ion-buttons start>
+          <button ion-button (click)="dismiss()">
+            <span ion-text color="primary" showWhen="ios">Cancel</span>
+            <ion-icon name="md-close" showWhen="android, windows"></ion-icon>
+          </button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <ion-slides pager>
+        <ion-slide *ngFor="let eventImg of eventDetail.imgList">
+          <img [src]="eventImg" class="slide-image"/>
+          <h2 class="slide-title" [innerHTML]="eventDetail.title"></h2>
+          <p [innerHTML]="eventDetail.description"></p>
+        </ion-slide>
+      </ion-slides>
+    </ion-content>
+  `
+})
+export class ModalContentPage {
+
+  eventDetail;
+
+  constructor(
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+    this.eventDetail = this.params.get('eventObj');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
